d6580b13e1486caf91b3e2c546177ba3d01621e3
DanYunShuYu <mac@MacdeiMac-61.local>
2016-03-12 15:02:27 -0800

    cha
    add  name

---
+++ GitHubDemo/GitHubDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 QinZhenZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
